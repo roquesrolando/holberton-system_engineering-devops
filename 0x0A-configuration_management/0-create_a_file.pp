9b28282956a3b093cffd9f33d95d8053c12d44d1
roquesrolando <roquesrolando@hotmail.com>
2021-08-13 23:10:43 +0000

    first commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file
+file { 'holberton':
+  ensure  => 'file',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
