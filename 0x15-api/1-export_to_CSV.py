cde2c10e83751d3084b35ebb60e7f3e092856a0f
roquesrolando <roquesrolando@hotmail.com>
2021-09-13 17:45:50 +0000

    fixes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,18 +5,11 @@ import sys
 
 if __name__ == "__main__":
 
-    count = 0
 
     ID = sys.argv[1]
     site = 'https://jsonplaceholder.typicode.com/'
     dos = requests.get(site + 'todos?userId={}'.format(ID)).json()
-    for i in dos:
-        total = total + 1
-        if i['completed'] is True:
-            count = count + 1
     user = requests.get(site + 'users/{}'.format(ID))
     name = user.json().get('name')
-    print('Employee {} is done with tasks({}/{}):'.format(name, count, total))
     for i in dos:
-        if i['completed'] is True:
-            print('\t {}'.format(i['title']))
+        print('"{}", "{}", "{}", "{}"'.format(ID, name, i['completed'], i['title']))
