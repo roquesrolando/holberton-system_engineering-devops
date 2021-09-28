72c64146838494c557db3ce41016999db1793746
roquesrolando <roquesrolando@hotmail.com>
2021-09-28 21:42:20 +0000

    rever

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
     agent = {'User-Agent': 'requested'}
     response = requests.get(url, headers=agent)
-    if response.status_code != '200':
+    if response.status_code in [302, 404]:
         print('None')
     else:
         posts = response.json()['data']['children']
