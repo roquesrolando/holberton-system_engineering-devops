17a8b6ce2037d53a58c86963995dbf275ba1194b
roquesrolando <roquesrolando@hotmail.com>
2021-09-28 22:05:32 +0000

    reverting one by one

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,7 @@ import requests
 def top_ten(subreddit):
     """Shows top ten post"""
     url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
-    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)\
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
-               Safari/537.36"}
+    headers = {'User-Agent': "requested"}
 
     response = requests.get(url, headers=headers)
     if response.status_code in [302, 404]:
