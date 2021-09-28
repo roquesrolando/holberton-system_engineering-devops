61aa18c6d1c2a81083dd7dfeb77b061d4915bae5
roquesrolando <roquesrolando@hotmail.com>
2021-09-28 21:45:28 +0000

    revert

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Gets data from an API"""
-import json
 import requests
 
 
@@ -9,11 +8,9 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
     agent = {'User-Agent': 'requested'}
     response = requests.get(url, headers=agent)
-    if response.status_code == 200:
-        response = response.json().get('data').get('children')
-
-        for post in response:
-            title = post.get('data').get('title')
-            print(title)
+    if response.status_code in [302, 404]:
+        print('None')
     else:
-        print("None")
+        posts = response.json()['data']['children']
+        for top in posts:
+            print(top['data']['title'])
