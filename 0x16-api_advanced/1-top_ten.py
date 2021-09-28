c6dfcd5f1b53aff4860c0dfe64b858614a20d553
roquesrolando <roquesrolando@hotmail.com>
2021-09-28 21:07:19 +0000

    added task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Gets data from an API"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """Shows top ten post"""
+    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    agent = {'User-Agent': 'requested'}
+    response = requests.get(url, headers=agent)
+    if response.status_code in [302, 404]:
+        print('None')
+    else:
+        posts = response.json()['data']['children']
+        for top in posts:
+            print(top['data']['title'])
