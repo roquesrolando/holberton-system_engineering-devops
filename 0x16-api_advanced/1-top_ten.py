8e959f1e5caa3af48f75742d55b08ad6f68e0111
roquesrolando <roquesrolando@hotmail.com>
2021-09-28 22:04:43 +0000

    reverting one by one

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines the following function:
-    top_ten(subreddit)
-"""
+"""Get data from an API"""
+import json
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit
-    """
+    """Shows top ten post"""
     url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
     headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)\
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
